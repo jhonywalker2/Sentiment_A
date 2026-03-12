0d1040ef494e62aad093743b4b76d6c8fa22735f
jhonywalker2 <168585042+jhonywalker2@users.noreply.github.com>
2026-03-12 10:41:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -36,9 +36,9 @@ with st.expander('Analizar texto'):
         x=round(blob.sentiment.polarity,2)
         if x > 0.0 and x <=1.0:
             st.write( 'Es un sentimiento Positivo 😊')
-          with open("Relieving Face Emoji.json") as source:
-              animation=json.load(source)
-          st.lottie(animation,width =350)
+            with open("Relieving Face Emoji.json") as source:
+                animation=json.load(source)
+            st.lottie(animation,width =350)
         elif x >=-1 and x <= 0:
             st.write( 'Es un sentimiento Negativo 😔')
         else:
